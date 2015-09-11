156dacd92262cc24bce03ca007a0b9092c1ea738
Classified56 <davis.ranney@MU-C1-01.mullen.pvt.k12.co.us>
2015-09-11 09:09:52 -0600

    Switch structure solved

--- MemoryGame/src/concentration.java
+++ MemoryGame/src/concentration.java
@@ -3,16 +3,70 @@ import java.io.*;
 
 public class concentration
 	{
-		
-		public static void main(String[] args)
+		static String guessArr[][] = new String[4][4];
+		static Scanner userGuess = new Scanner(System.in);
+		static Scanner file;
+		public static void main(String[] args)throws IOException
 			{
-				
-
+				//initializeTempTheme();
+				arrayTheme();
+				makeGrid();
+			}
+		public static void initializeTempTheme()throws IOException
+			{
+				file = new Scanner(new File("animals.txt"));
+				for(int i = 0; i < 2; i++)
+					{
+						for(int j = 0; j < 4; j++)
+							{
+								guessArr[i][j] = file.nextLine();
+							}
+					}
+				for(int i = 2; i < 4; i++)
+					{
+						for(int j = 0; j < 4; j++)
+							{
+								guessArr[i][j] = guessArr[i - 2][j];
+							}
+					}
 			}
 		public static void makeGrid()
 			{
-				System.out.println("|      |      |      |      |");
-				
+				for(int i = 0; i < 3; i++)
+					{
+						System.out.println("| " + guessArr[i][0] + " | " + guessArr[i][1] + " | " + guessArr[i][2] + " | " + guessArr[i][3] + " |");
+						System.out.println("-----------------------------");
+					}
+				System.out.println("| " + guessArr[3][0] + " | " + guessArr[3][1] + " | " + guessArr[3][2] + " | " + guessArr[3][3] + " |");
+			}
+		public static void arrayTheme() throws IOException
+			{
+				System.out.print("What theme would you like? (a. Animals b. Household Items)");
+				String themeSel = userGuess.next();
+				char userLetter = themeSel.charAt(0);
+				switch(userLetter)
+					{
+						case 'a': 
+							file = new Scanner(new File("animals.txt")); 
+							break;
+						case 'b': 
+							file = new Scanner(new File("housethings.txt"));
+							break;
+					}
+				for(int i = 0; i < 2; i++)
+					{
+						for(int j = 0; j < 4; j++)
+							{
+								guessArr[i][j] = file.nextLine();
+							}
+					}
+				for(int i = 2; i < 4; i++)
+					{
+						for(int j = 0; j < 4; j++)
+							{
+								guessArr[i][j] = guessArr[i - 2][j];
+							}
+					}
 			}
 
 	}
